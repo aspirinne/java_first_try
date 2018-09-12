4b8451be785a49b4c357cd632968ee3f2337289c
БО2 Стажер <bo2.stazher@bars-open.ru>
2018-09-12 15:33:13 +0300

    complete all

--- src/main/java/system/controller/UserController.java
+++ src/main/java/system/controller/UserController.java
@@ -2,8 +2,11 @@ package system.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 import system.model.User;
 import system.service.UserService;
 
@@ -16,9 +19,25 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    public @ResponseBody
-    List<User> getAllUsers(){
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public @ResponseBody List<User> getAllUsers(){
         return userService.getAllUsers();
+    }
+
+    @RequestMapping(value = "/validate", method = RequestMethod.GET)
+    public ModelAndView validateUser(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("userFromServer", new User());
+        modelAndView.setViewName("users_check_page");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/check", method = RequestMethod.POST)
+    public @ResponseBody String checkUser(@ModelAttribute("userFromServer") User user){
+        if ("admin".equals(user.getName()) && "admin".equals(user.getPassword())){
+            return "valid";
+        }
+        return "invalid";
     }
 
 }
